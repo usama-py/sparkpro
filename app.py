1573976999880b05f29e6d96a07aa1644b748eb6
mohd usama <mohd.usama984@gmail.com>
2022-06-09 22:49:11 +0530

    new db url

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ import MySQLdb.cursors
 #port = int(os.getenv('PORT'))
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/bank'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iiyvonnsewprdv:6311257ea413cd9965b89fd760fc72e523e8b06f6d1dfa41459601b6d8a52a7d@ec2-52-73-184-24.compute-1.amazonaws.com:5432/dc3irl4ve4ffbl'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://:sql6498675:wrJmLMRQMS@sql6.freemysqlhosting.net:3306/sql6498675'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iiyvonnsewprdv:6311257ea413cd9965b89fd760fc72e523e8b06f6d1dfa41459601b6d8a52a7d@ec2-52-73-184-24.compute-1.amazonaws.com:5432/dc3irl4ve4ffbl'
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
