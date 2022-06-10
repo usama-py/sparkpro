2db8140dbc058b7b649a3beb29c460dde7f49ece
mohd usama <mohd.usama984@gmail.com>
2022-06-10 21:56:51 +0530

    final commit for git

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
+#All imports needed
 from datetime import datetime
 from flask_migrate import Migrate
 from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
-
 import MySQLdb.cursors
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://sql6498675:wrJmLMRQMS@sql6.freemysqlhosting.net:3306/sql6498675'
 
-app.config['MYSQL_DB'] = 'sql6498675'
-app.config['MYSQL_HOST'] = 'sql6.freemysqlhosting.net'
+#initializing application
+app = Flask(__name__)
+#Removed the orignal credentials after deployment on heroku
+#This is just the format for credentials
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://<mysql user here >:<mysql password here>@<mysql db host here>:<port no here>/<db name here>'
+#MySQL DB name
+app.config['MYSQL_DB'] = 'db name here'
+#MySQL Host name
+app.config['MYSQL_HOST'] = 'host name here'
 #MySQL username
-app.config['MYSQL_USER'] = 'sql6498675'
-app.config['MYSQL_PASSWORD'] = 'wrJmLMRQMS'
+app.config['MYSQL_USER'] = 'user name here'
+#MySQL Password
+app.config['MYSQL_PASSWORD'] = 'password here'
 mysql = MySQL(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+#Can use this if needed
 
 # if __name__ == "__main__":
 #     app.run(host=os.getenv('IP', '0.0.0.0'),
 #             port=int(os.getenv('PORT', 4444)))
 
+#Transaction management table class, contains all the columns of transaction
 class Transaction_info(db.Model):
     cust_id = db.Column(db.Integer, unique=False, nullable=False)
     amt_withdrawn = db.Column(db.Integer,unique=False, nullable=False)
@@ -33,12 +41,13 @@ class Transaction_info(db.Model):
         return '<User %r>' % self.username
 
 
-
+#Home route, first page to load on url
 @app.route("/")
 def home():
     return render_template("finances-master/index.html")
 
 
+#Page to view all the customers data present in the database
 @app.route("/view-all-customers",methods=['GET','POST'])
 def viewCustomers():
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -48,28 +57,28 @@ def viewCustomers():
     data = cursor.fetchall()
     cursor.close()
 
-
-
     return render_template("finances-master/customers.html", data=data)
 
+#logic for transactions between selected users
 @app.route("/transfer-money",methods=['GET','POST'])
 def transfer():
-
+    #when post request is sent
     if request.method == 'POST':
         '''Add entry to the database'''
+        #gets ids of sender and receiver and the amount
         cust_id = request.form.get('sender')
         receiver_id = request.form.get('receiver')
         amt_withdrawn = request.form.get('amount')
         cust_id = int(cust_id)
         receiver_id = int(receiver_id)
         amt_withdrawn = int(amt_withdrawn)
-        app.config['MYSQL_USER'] = 'sql6498675'
+        #records and commits the transacttion into the database
         entry = Transaction_info(cust_id=cust_id, receiver_id=receiver_id, amt_withdrawn=amt_withdrawn, withdraw_time=datetime.utcnow())
         db.session.add(entry)
         db.session.commit()
         conn = mysql.connection
         cursor = conn.cursor()
-
+        #updates the balance for the sender and receiver
         cursor.execute('select balance from customers_info where cust_id = %s', (cust_id,))
         amt_withdrawn = int(amt_withdrawn)
         balance1 = cursor.fetchone()
@@ -84,14 +93,17 @@ def transfer():
         conn.commit()
         stmt2 = "update customers_info set balance = %s where cust_id = %s"
         cursor.execute(stmt2, (y, receiver_id,))
-
-
+        #values are commited in customer table
         conn.commit()
         cursor.close()
         return render_template("finances-master/redirect.html")
 
     return render_template("finances-master/transfer.html")
 
+#Redirect back to customers table to confirm the transaction yourself
 @app.route("/redirect-to-customers")
 def redirect():
     return render_template("finances-master/redirect.html")
+
+#Not the best code but a try with flask which is not so common on web development
+#Just a skeleton structure for any other improvements
